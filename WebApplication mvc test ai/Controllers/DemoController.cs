38ade882233ea74ee2eb657643bf86caa15da918
Navid Shiraj <navid2007@hotmail.com>
2023-08-07 00:24:59 +0100

    add front end application insights

---
+++ WebApplication mvc test ai/Controllers/DemoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication_mvc_test_ai.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        private readonly string url = "<function_url>";
+
+        public DemoController()
+        { }
+
+        [HttpGet]
+        public async Task<ActionResult<string>> Get()
+        {
+            //HttpClient client = new HttpClient();
+            //var response = await client.GetAsync(url);
+            //var text = await response.Content.ReadAsStringAsync();
+
+            return new OkObjectResult("hello word");
+        }
+
+    }
+}
