32ce1880de65c1b742140702b3744e2329eed954
Navid Shiraj <navid2007@hotmail.com>
2023-09-04 20:02:46 +0100

    Display total from workersservice with signalR

--- ServiceBusConsumer/CalculatorConsumer.cs
+++ ServiceBusConsumer/CalculatorConsumer.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.SignalR;
 using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.Azure.Amqp.Framing;
 using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using System.Text;
 //using WebApplication_mvc_test_ai.Hubs;
 
 namespace ServiceBusConsumer
@@ -21,10 +23,8 @@ namespace ServiceBusConsumer
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-
             var hubConnection = new HubConnectionBuilder().WithUrl("https://localhost:7084/chatHub").Build();
             await hubConnection.StartAsync();
-
 
             hubConnection.On<string, string>("ReceiveMessage", (user, message) =>
             {
@@ -32,44 +32,26 @@ namespace ServiceBusConsumer
                 message = user + message;
             });
 
-            while (!stoppingToken.IsCancellationRequested)
+            var total = 0;
+            _subscriptionClient.RegisterMessageHandler(async (message, tokent) =>
             {
-                _logger.LogInformation("Worker running at: {Time}", DateTime.Now);
-                await hubConnection.SendAsync("SendMessage", "workerservice ", "10");
-                await Task.Delay(3000, stoppingToken);
-            }
+                var number = JsonConvert.DeserializeObject<int>(Encoding.UTF8.GetString(message.Body));
+                 total = 150 + number;
+
+                _logger.LogInformation("number is: {number} and we add 150 to get: {total}", number, total);
+
+                await _subscriptionClient.CompleteAsync(message.SystemProperties.LockToken);
+                await hubConnection.SendAsync("SendMessage", "Workerservice ", total.ToString());
+
+            }, new MessageHandlerOptions(args => Task.CompletedTask)
+            {
+                AutoComplete = false,
+                MaxConcurrentCalls = 1,
+            });
+
             await hubConnection.DisposeAsync();
+            await Task.CompletedTask;
         }
-
-
-
-
-
-
-        //protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        //{
-        //    Random random = new Random();
-        //    int randomNumber = random.Next(101);
-        //    await _hubContext.Clients.All.SendAsync("ReceiveMessage", $"Hello World {randomNumber}");
-
-        //    _subscriptionClient.RegisterMessageHandler(async (message, tokent) =>
-        //    {
-        //        var number = JsonConvert.DeserializeObject<int>(Encoding.UTF8.GetString(message.Body));
-        //        var total = 150 + number;
-
-        //        _logger.LogInformation("number is: {number} and we add 150 to get: {total}", number, total);
-        //        //_hubContext.Clients.All.SendAsync("ReceiveMessage","dd", (number + 150).ToString());
-
-        //        await _subscriptionClient.CompleteAsync(message.SystemProperties.LockToken);
-
-        //    }, new MessageHandlerOptions(args => Task.CompletedTask)
-        //    {
-        //        AutoComplete = false,
-        //        MaxConcurrentCalls = 1,
-        //    }) ;
-
-        //    await Task.CompletedTask;
-        //}
 
 
     }
